0f8fa1b79757ac4774deda3f0b2be3d5d914d145
Partho P. Das <parthopdas@live.com>
2021-01-18 11:41:13 +1000

    Intial commit...

---
+++ build/jest.config.d.ts
@@ -0,0 +1,3 @@
+import type { Config } from '@jest/types';
+declare const initialOptions: () => Promise<Config.InitialOptions>;
+export default initialOptions;
---
+++ build/src/TestComponent/TestComponent.d.ts
@@ -0,0 +1,4 @@
+import { TestComponentProps } from './TestComponent.types';
+import './TestComponent.scss';
+declare const TestComponent: ({ theme }: TestComponentProps) => JSX.Element;
+export default TestComponent;
